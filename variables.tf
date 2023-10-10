681ec1fedc088d660cf936c9975e4bc32b80bb44
Kraig McFadden <kraig@shiftsmart.com>
2023-10-10 15:41:02 -0400

    chore: formatting

--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 /* Required */
 variable "topic_name" {
-    type = string
+  type = string
 }
 
 variable "environment" {
-    type = string
-    description = "prod or staging"
+  type        = string
+  description = "prod or staging"
 }
 
 variable "service_account_members" {
-    type = list(string)
-    description = "List of service account members (of the format serviceAccount:{email}) to grant permission to publish to this topic"
+  type        = list(string)
+  description = "List of service account members (of the format serviceAccount:{email}) to grant permission to publish to this topic"
 }
 
 /* Optional */
 variable "retention" {
-    type = string
-    default = "604800s"
-    description = "How long the topic should retain messages for. Default is 7 days."
+  type        = string
+  default     = "604800s"
+  description = "How long the topic should retain messages for. Default is 7 days."
 }
